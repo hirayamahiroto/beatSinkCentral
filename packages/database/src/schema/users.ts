013ffd860f8163d134a37eae49ef2302e21e826c
hirayamahiroto <h.hirayama@ei-shin.com>
2025-09-28 22:46:05 +0900

    ✨ UsersSchemaを修正しました。

--- packages/database/src/schema/users.ts
+++ packages/database/src/schema/users.ts
@@ -1,31 +1,18 @@
 import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm";
-import {
-  createInsertSchema,
-  createSelectSchema,
-  createUpdateSchema,
-} from "drizzle-zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { userRolesTable } from "./userRoles";
 
 export const usersTable = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
   roleId: uuid("role_id")
     .notNull()
     .references(() => userRolesTable.id),
-  name: varchar("name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  password: varchar("password", { length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  deletedAt: timestamp("deleted_at"),
 });
-
-export const usersRelations = relations(usersTable, ({ one }) => ({
-  role: one(userRolesTable, {
-    fields: [usersTable.roleId],
-    references: [userRolesTable.id],
-  }),
-}));
 
 export const userSelectSchema = createSelectSchema(usersTable);
 export const userInsertSchema = createInsertSchema(usersTable);
-export const userUpdateSchema = createUpdateSchema(usersTable);
