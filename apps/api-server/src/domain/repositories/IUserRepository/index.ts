de848706638258dc0c88851169362590503505f0
hirayamahiroto <h.hirayama@ei-shin.com>
2025-10-13 16:15:16 +0900

    🔥使用していない関数を削除

--- apps/api-server/src/domain/repositories/IUserRepository/index.ts
+++ apps/api-server/src/domain/repositories/IUserRepository/index.ts
@@ -14,17 +14,7 @@ export interface IUserRepository {
   create(dto: CreateUserDto): Promise<User>;
 
   /**
-   * IDでユーザーを取得
-   */
-  findById(id: string): Promise<User | null>;
-
-  /**
    * Auth0ユーザーIDでユーザーを取得
    */
   findByAuth0UserId(auth0UserId: string): Promise<User | null>;
-
-  /**
-   * メールアドレスでユーザーを取得
-   */
-  findByEmail(email: string): Promise<User | null>;
 }
