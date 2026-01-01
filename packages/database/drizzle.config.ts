e77fe0cb0787f79e20666c84f275d375f3f47b22
hirayamahiroto <hirayamahiroto@users.noreply.github.com>
2026-01-01 22:55:52 +0900

    修正

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,14 +1,3 @@
-// import { defineConfig } from "drizzle-kit";
-
-// export default defineConfig({
-//   out: "./drizzle/migrations",
-//   schema: "./src/schema/*.ts",
-//   dialect: "postgresql",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL!,
-//   },
-// });
-
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
